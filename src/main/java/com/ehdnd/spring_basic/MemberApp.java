05ed5d6a3302bc5d4f6f65b5e59dc9891293a265
ehdnd_desktop <taeungjang@naver.com>
2026-02-09 15:28:09 +0900

    3.13 회원 도메인 실행과 테스트

---
+++ src/main/java/com/ehdnd/spring_basic/MemberApp.java
@@ -0,0 +1,20 @@
+package com.ehdnd.spring_basic;
+
+import com.ehdnd.spring_basic.member.Grade;
+import com.ehdnd.spring_basic.member.Member;
+import com.ehdnd.spring_basic.member.MemberService;
+import com.ehdnd.spring_basic.member.MemberServiceImpl;
+
+public class MemberApp {
+
+  public static void main(String[] args) {
+    MemberService memberService = new MemberServiceImpl();
+    Member memberA = new Member(1L, "memberA", Grade.VIP);
+    memberService.join(memberA);
+
+    Member findMember = memberService.findMember(1L);
+    System.out.println("findMember = " + findMember.getName());
+    System.out.println("memberA = " + memberA.getName());
+  }
+
+}
