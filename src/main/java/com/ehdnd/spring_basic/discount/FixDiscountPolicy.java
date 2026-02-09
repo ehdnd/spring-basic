e09c5d30ec2e13e6278853cb08242bb626215b9f
ehdnd_desktop <taeungjang@naver.com>
2026-02-09 16:22:35 +0900

    3.15 주문과 할인 도메인 개발

---
+++ src/main/java/com/ehdnd/spring_basic/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package com.ehdnd.spring_basic.discount;
+
+import com.ehdnd.spring_basic.member.Grade;
+import com.ehdnd.spring_basic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+  //    1000원 할인
+  private int discountFixAmount = 1000;
+
+  @Override
+  public int discount(Member member, int price) {
+    if (member.getGrade() == Grade.VIP) {
+      return discountFixAmount;
+    } else {
+      return 0;
+    }
+  }
+}
