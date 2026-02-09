93ba37fa7f03450c51fadc99072caddec9de9e88
ehdnd_desktop <taeungjang@naver.com>
2026-02-09 21:08:54 +0900

    4.20 AppConfig 리팩터링
    근데 이러면 객체가 다 따로 생기는거 아닌가>?

--- src/main/java/com/ehdnd/spring_basic/AppConfig.java
+++ src/main/java/com/ehdnd/spring_basic/AppConfig.java
@@ -1,6 +1,8 @@
 package com.ehdnd.spring_basic;
 
+import com.ehdnd.spring_basic.discount.DiscountPolicy;
 import com.ehdnd.spring_basic.discount.FixDiscountPolicy;
+import com.ehdnd.spring_basic.member.MemberRepository;
 import com.ehdnd.spring_basic.member.MemberService;
 import com.ehdnd.spring_basic.member.MemberServiceImpl;
 import com.ehdnd.spring_basic.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.ehdnd.spring_basic.order.OrderServiceImpl;
 public class AppConfig {
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  private MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  private DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 
 }
