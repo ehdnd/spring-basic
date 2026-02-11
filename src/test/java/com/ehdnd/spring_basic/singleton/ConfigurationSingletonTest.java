6fe786ee8086f11dad73d025d02ec2e04badf329
ehdnd_desktop <taeungjang@naver.com>
2026-02-12 00:14:40 +0900

    6.38 @Configuration과 싱글톤

---
+++ src/test/java/com/ehdnd/spring_basic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,38 @@
+package com.ehdnd.spring_basic.singleton;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.ehdnd.spring_basic.AppConfig;
+import com.ehdnd.spring_basic.member.MemberRepository;
+import com.ehdnd.spring_basic.member.MemberServiceImpl;
+import com.ehdnd.spring_basic.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+  @Test
+  void configurationTest() {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    MemberServiceImpl memberService = ac.getBean("memberService",
+        MemberServiceImpl.class);
+    OrderServiceImpl orderService = ac.getBean("orderService",
+        OrderServiceImpl.class);
+    MemberRepository memberRepository = ac.getBean("memberRepository",
+        MemberRepository.class);
+
+    // 모두 같은 인스턴스를 참고하고 있다.
+    System.out.println("memberService -> memberRepository = " +
+        memberService.getMemberRepository());
+    System.out.println("orderService -> memberRepository  = " +
+        orderService.getMemberRepository());
+    System.out.println("memberRepository = " + memberRepository);
+
+    //모두 같은 인스턴스를 참고하고 있다.
+    assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+    assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+  }
+
+}
