3c9cebee7d37c7b97b47121459528b45db64c0ea
ehdnd_desktop <taeungjang@naver.com>
2026-02-11 23:40:42 +0900

    6.36 싱글톤 컨테이너

--- src/test/java/com/ehdnd/spring_basic/singleton/SingletonTest.java
+++ src/test/java/com/ehdnd/spring_basic/singleton/SingletonTest.java
@@ -6,6 +6,7 @@ import com.ehdnd.spring_basic.AppConfig;
 import com.ehdnd.spring_basic.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -13,6 +14,7 @@ public class SingletonTest {
   @DisplayName("스프링 없는 순수한 DI 컨테이너")
   void pureContainer() {
     AppConfig appConfig = new AppConfig();
+
     //1. 조회: 호출할 때 마다 객체를 생성
     MemberService memberService1 = appConfig.memberService();
     //2. 조회: 호출할 때 마다 객체를 생성
@@ -47,6 +49,23 @@ public class SingletonTest {
     assertThat(singletonService1).isSameAs(singletonService2);
 
     singletonService1.logic();
+  }
+
+  @Test
+  @DisplayName("스프링 컨테이너와 싱글톤")
+  void springContainer() {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+        AppConfig.class);
+
+    MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+    MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+    //참조값이 같은 것을 확인
+    System.out.println("memberService1 = " + memberService1);
+    System.out.println("memberService2 = " + memberService2);
+
+    //memberService1 == memberService2
+    assertThat(memberService1).isSameAs(memberService2);
   }
 
 }
