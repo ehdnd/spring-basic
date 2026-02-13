221ac92771eb1ab8120162385e1d2c5e27a49231
ehdnd_desktop <taeungjang@naver.com>
2026-02-13 21:44:58 +0900

    8.47 롬복과 최신 트렌드
    1. build.gradle 에 lombok 설치
    2. lombok 플러그인 설치
    3. 설정 - annotation processors - enable annotation processing

--- src/main/java/com/ehdnd/spring_basic/order/OrderServiceImpl.java
+++ src/main/java/com/ehdnd/spring_basic/order/OrderServiceImpl.java
@@ -3,23 +3,20 @@ package com.ehdnd.spring_basic.order;
 import com.ehdnd.spring_basic.discount.DiscountPolicy;
 import com.ehdnd.spring_basic.member.Member;
 import com.ehdnd.spring_basic.member.MemberRepository;
-import com.ehdnd.spring_basic.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
   // 한 번 설정하면 바뀌지 않는, 불변 필수 의존관계 - 생성자 주입 (setter 메서드 만들면 안되겠지.)
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
 
-  // 생성자가 하나만 있으면 @Autowired 생략이 가능하다.
-  @Autowired
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
+  // 생성자를 하나만 두면 @Autowired 생략이 가능하므로,
+  // 생성자를 한개만 두고 Lombok 라이브러리의 @RequiredArgsConstructor 사용하여 깔끔하게 하자.
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
