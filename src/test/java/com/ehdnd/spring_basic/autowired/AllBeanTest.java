e9576f4acecec32d2cf0e9763f64f0b767ec604c
ehdnd_rokaf <taeungjang@naver.com>
2026-07-17 05:45:41 +0000

    7장까지 복기 완료

--- src/test/java/com/ehdnd/spring_basic/autowired/AllBeanTest.java
+++ src/test/java/com/ehdnd/spring_basic/autowired/AllBeanTest.java
@@ -40,7 +40,8 @@ public class AllBeanTest {
     private final List<DiscountPolicy> policies;
 
     // 생성자 파라미터를 Map/List<DiscountPolicy>로 선언하면,
-    // 스프링이 DiscountPolicy의 모든 구현 빈을 찾아 컬렉션으로 주입한다.
+    // 스프링이 DiscountPolicy의 모든 구현 빈을 찾아 컬렉션으로 주입
+    // = 전략 패턴
     // 다만, 다형성을 적극 활용하는 비즈니스 로직은, 수동 등록을 고민하자.
     public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
       this.policyMap = policyMap;
@@ -49,6 +50,11 @@ public class AllBeanTest {
       System.out.println("policies = " + policies);
     }
 
+    // discountCode는 Map의 key.
+    // 현 상태에서 key를 찾으려면..
+    // DiscountPolicy를 impl하는 클래스 - `@Component` 확인 - 이름 확인 - 다른 곳도 확인 ..
+    // -> 수동 등록이라면 DiscountPolicyConfig 하나만 보면 된다.
+    // -> 최소한 discount/ 패키지 하나에 넣어두어라.
     public int discount(Member member, int price, String discountCode) {
       DiscountPolicy discountPolicy = policyMap.get(discountCode);
       return discountPolicy.discount(member, price);
