3d09ddac989cf7ab778762719bce68562267347d
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-07-19 07:57:03 +0000

    9.54 인터페이스 InitiallizingBean, DisposableBean
    스프링 전용 인터페이스에 의존

--- src/test/java/com/ehdnd/spring_basic/lifecycle/NetworkClient.java
+++ src/test/java/com/ehdnd/spring_basic/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package com.ehdnd.spring_basic.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.DisposableBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
 
@@ -26,6 +29,19 @@ public class NetworkClient {
   // 서비스 종료 시 호출
   public void disconnect() {
     System.out.println("close: " + url);
+  }
+
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    connect();
+    call("초기화 연결 메시지");
+  }
+
+
+  @Override
+  public void destroy() throws Exception {
+    disconnect();
   }
 }
 
