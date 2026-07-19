156a1e82065108f8578bd68504546565add2442d
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-07-19 07:34:39 +0000

    9.53 빈 생명주기 콜백 시작

---
+++ src/test/java/com/ehdnd/spring_basic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package com.ehdnd.spring_basic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+  // 스프링 컨테이너 생성 - 스프링 빈 생성 - 의존관계 주입 - 초기화 콜백 - 사용 - 소멸전 콜백- 스프링 종료
+
+  @Test
+  public void lifeCycleTest() {
+    ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(
+        LifeCycleConfig.class);
+    NetworkClient client = ac.getBean(NetworkClient.class);
+    ac.close();
+  }
+
+  @Configuration
+  static class LifeCycleConfig {
+
+    @Bean
+    public NetworkClient networkClient() {
+      NetworkClient networkClient = new NetworkClient(); // 객체 생성 (파라미터)
+      networkClient.setUrl("http://dev.dev"); // 초기화 (무거운) - 두 작업을 분리
+      return networkClient;
+    }
+  }
+
+}
