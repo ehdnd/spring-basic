0dfeb805a0b0d20f79ec257186eada3e05896f42
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-07-24 09:15:57 +0000

    9.62 request 스코프 예제 만들기
    에러: Scope 'request' is not active for the current thread;
    HTTP 요청 ~ 끝 까지가 생존 범위라 스프링 컨테이너 실행시에는 당연히 생존 범위가 아니다. 실제 고객의 요청이 와야 생성할 수 있다. -> 의존관계 설정을 뒤로 미뤄야 한다. -> Provider를 사용하자.

---
+++ src/main/java/com/ehdnd/spring_basic/common/MyLogger.java
@@ -0,0 +1,32 @@
+package com.ehdnd.spring_basic.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.util.UUID;
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("request") // HTTP 요청 당 각각 구분된다.
+public class MyLogger {
+
+  private String uuid;
+  @Setter
+  private String requestURL;
+
+  public void log(String message) {
+    System.out.println("[" + uuid + "] " + "[" + requestURL + "] " + message);
+  }
+
+  @PostConstruct
+  public void init() {
+    uuid = UUID.randomUUID().toString();
+    System.out.println("[" + uuid + "] request scope bean create: " + this);
+  }
+
+  @PreDestroy
+  public void destory() {
+    System.out.println("[" + uuid + "] request scope bean close: " + this);
+  }
+}
---
+++ src/main/java/com/ehdnd/spring_basic/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package com.ehdnd.spring_basic.web;
+
+import com.ehdnd.spring_basic.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+  private final LogDemoService logDemoService;
+  private final MyLogger myLogger;
+
+  @RequestMapping("log-demo")
+  @ResponseBody
+  public String logDemo(HttpServletRequest request) {
+    String requestURL = request.getRequestURL().toString();
+    myLogger.setRequestURL(requestURL); // 스프링 인터셉터를 사용하는 것이 좋다.
+
+    myLogger.log("controller test");
+    logDemoService.logic("testId");
+
+    return "OK";
+  }
+
+}
