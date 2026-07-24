ca78be91238d0ff55a013ea33d300a75d46080e3
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-07-24 06:37:45 +0000

    9.59 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점
    싱글톤 빈이 프로토타입 빈을 사용하게 된다.
    생성 시점에만 의존관계 주입을 받으므로
    의도와 다르게 프로토타입 빈이 새로 생성되지 않는다.

---
+++ src/test/java/com/ehdnd/spring_basic/scope/SingletonWithPrototypeTest.java
@@ -0,0 +1,81 @@
+package com.ehdnd.spring_basic.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest {
+
+  @Test
+  void prototypeFind() {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+        PrototypeBean.class);
+
+    PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+    PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+    prototypeBean1.addCount();
+    assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+    prototypeBean2.addCount();
+    assertThat(prototypeBean2.getCount()).isEqualTo(1);
+  }
+
+  @Test
+  void singletonClientUsePrototype() {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,
+        PrototypeBean.class);
+
+    ClientBean clientBean1 = ac.getBean(ClientBean.class);
+
+    int count1 = clientBean1.logic();
+    assertThat(count1).isEqualTo(1);
+
+    int count2 = clientBean1.logic(); // 사용할 때마다 새로 생성되지 않는다.
+    assertThat(count2).isEqualTo(2); // 우리는 새로 생성되는 것을 원하므로 의도와 다르게 동작한다.
+  }
+
+  @Scope("singleton")
+  static class ClientBean {
+
+    private final PrototypeBean prototypeBean; // 생성 시점에 주입
+
+    ClientBean(PrototypeBean prototypeBean) {
+      this.prototypeBean = prototypeBean;
+    }
+
+    public int logic() {
+      prototypeBean.addCount();
+      return prototypeBean.getCount();
+    }
+  }
+
+  @Scope("prototype")
+  static class PrototypeBean {
+
+    private int count = 0;
+
+    public void addCount() {
+      ++count;
+    }
+
+    public int getCount() {
+      return count;
+    }
+
+    @PostConstruct
+    public void init() {
+      System.out.println("PrototypeBean init" + this);
+    }
+
+    @PreDestroy // 호출 안되긴 한다.
+    public void destroy() {
+      System.out.println("PrototypeBean destroy");
+    }
+
+  }
+}
